d002ee90d85d6532eb254470f17a5acb0a275985
nir099 <a.m.s.nirmal.a@gmail.com>
2019-08-23 17:01:10 +0530

    kkk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,32 @@
-import React from "react"
+import React , { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+class IndexPage extends Component { 
+
+  componentDidMount () {
+    const script = document.createElement("script");
+  
+    script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+    script.async = true;
+  
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      })
+    }
+  
+    document.body.appendChild(script);
+  }
+
+  render () { return (
   <Layout>
     <SEO title="Home" />
     <h1>Crash it</h1>
@@ -16,5 +37,7 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+  }
+}
 
 export default IndexPage
