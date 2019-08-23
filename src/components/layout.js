4ee7a49e27ed079d36070a5dfd7310ab1f253e53
nir099 <a.m.s.nirmal.a@gmail.com>
2019-08-23 16:46:26 +0530

    dnk

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import Header from "./header"
 import Menu from "./menu"
 import "./layout.css"
 
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -26,6 +27,18 @@ const Layout = ({ children }) => {
 
   return (
     <div>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    <script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    })
+  }
+</script>
       <Header siteTitle={data.site.siteMetadata.title} />
       <Menu />
       <div
